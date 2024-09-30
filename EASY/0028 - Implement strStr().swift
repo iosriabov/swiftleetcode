86d3b540298ce84d23f10665383fc53a0575eaea
Vladimir Riabov <109576155+iosriabov@users.noreply.github.com>
2024-09-30 18:32:00 +0300

    Update 0028 - Implement strStr().swift

--- EASY/0028 - Implement strStr().swift
+++ EASY/0028 - Implement strStr().swift
@@ -1,5 +1,12 @@
 class Solution {
     func strStr(_ haystack: String, _ needle: String) -> Int {
+      guard let range = haystack.range(of: needle) else { return -1 }
+      return haystack.distance(from: haystack.startIndex, to: range.lowerBound)
+    }
+}
+
+class Solution {
+    func strStr(_ haystack: String, _ needle: String) -> Int {
         if haystack.count == needle.count {
             return haystack != needle ? -1 : 0
         } else if let index = haystack.range(of: needle) {
